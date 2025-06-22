d9540f40ff76b4ae2505299367161ab650b5ee23
Pooja Sain <sainpooju567@gmail.com>
2025-06-23 00:36:58 +0530

    complete context

--- e-commerce/src/components/CartSummary.jsx
+++ e-commerce/src/components/CartSummary.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import AppContext from "../Context/AppContext";
+import CartItem from "./CartItem";
 
 const CartSummary = () => {
-  return <div>CartSummary</div>;
+  const { cart } = useContext(AppContext);
+  const { cartItems, total } = cart;
+
+  return (
+    <>
+      <h1>Cart</h1>
+
+      <div className="cart-items mb-4">
+        {cartItems.map((cartItem) => (
+          <CartItem details={cartItem} key={cartItem.id} />
+        ))}
+      </div>
+
+      <div className="total d-flex align-items-center justify-content-between bg-light text-dark">
+        <h2 className="mb-3">Total</h2>
+        <h4>: ₹{total.toFixed(2)}</h4>
+      </div>
+    </>
+  );
 };
 
 export default CartSummary;
