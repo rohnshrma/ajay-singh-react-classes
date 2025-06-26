920eb1e50f6108976cd9c6e0f1e52e64afa7b341
rohnshrma <rohn11shrma@gmail.com>
2025-06-26 12:30:23 +0530

    fix: Correct product ID parsing and improve dish rendering logic

--- food order app/src/components/Product.jsx
+++ food order app/src/components/Product.jsx
@@ -4,8 +4,12 @@ import { useParams } from "react-router-dom";
 const Product = () => {
   const { menu } = useContext(CartContext);
   const { id } = useParams();
+  const productId = parseInt(id);
 
-  const dish = menu && menu.find((dish) => dish.id === id);
+  console.log("product page => ", menu, productId);
+
+  const dish = menu && menu.find((dish) => dish.id === productId);
+  console.log(dish);
   if (dish) {
     return (
       <div className="product d-flex justify-content-center">
@@ -13,17 +17,17 @@ const Product = () => {
           <img src={`${dish.image}`} alt={dish.name} />
         </div>
         <div className="col-lg-7">
-          <div className="d-flex justify-content-between">
+          <div className="">
             <h4>
               {dish.category}/{dish.subCategory}
             </h4>
             <h2>{dish.name}</h2>
             <span
               className={`badge ${
-                vegNonVeg === "veg" ? "bg-success" : "bg-danger"
+                dish.vegNonVeg === "veg" ? "bg-success" : "bg-danger"
               } mb-2 fs-6`}
             >
-              {vegNonVeg}
+              {dish.vegNonVeg}
             </span>
           </div>
           <p>{dish.description}</p>
